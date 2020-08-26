63f607c73b70bac283f69f5907649a9310af5d7b
M3D5 <pablohdzjr@gmail.com>
2020-08-26 00:21:26 -0500

    removed everything, using friend exercise as example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./utils/employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    employees,
+  };
+
+  removeEmployee = (id) => {
+    // Filter this.state.employees for employees with an id not equal to the id being removed
+    const employees = this.state.employees.filter(
+      (employee) => employee.id !== id
+    );
+    // Set this.state.employees equal to the new employees array
+    this.setState({ employees });
+  };
+
+  // Map over this.state.employees and render a employeeCard component for each employee object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee List</Title>
+        {this.state.employees.map((employee) => (
+          <EmployeeCard
+            removeemployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
