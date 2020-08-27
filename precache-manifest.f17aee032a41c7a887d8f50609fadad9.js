f23ad831fd3acd4ca47e93d70287e045ce504ff0
M3D5 <pablohdzjr@gmail.com>
2020-08-26 23:33:05 -0500

    Updates

---
+++ precache-manifest.f17aee032a41c7a887d8f50609fadad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd180fef188d9149330da6c1ca2d61e",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "fdea8f87ef45df6c7cd6",
+    "url": "/react-employee-directory/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e4fa0bc3161353f385ab",
+    "url": "/react-employee-directory/static/css/main.778bac7d.chunk.css"
+  },
+  {
+    "revision": "fdea8f87ef45df6c7cd6",
+    "url": "/react-employee-directory/static/js/2.459f0d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.459f0d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fa0bc3161353f385ab",
+    "url": "/react-employee-directory/static/js/main.065e9c64.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
